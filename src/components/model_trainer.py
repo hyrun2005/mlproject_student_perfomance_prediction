a1a7265de86c8b69967fa079ae4d80fbbc3d1bba
Yurii Hyrun <hyrun2005@gmail.com>
2025-03-19 20:53:49 +0100

    Add hyperparameters

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -46,8 +46,38 @@ class modelTraining:
                 "AdaBoost Regression": AdaBoostRegressor()
             }
 
+            params={
+                "Random Forest":{
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Decision Tree": {
+                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                },
+                "Gradient Descent":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Linear Regression":{},
+                "K-Neighbors Classifier": {
+                    'algorithm':['auto', 'ball_tree', 'kd_tree', 'brute']
+                },
+                "XGBClassifier":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "CatBoosting Regression":{
+                    'depth': [6,8,10],
+                    'learning_rate': [0.01, 0.05, 0.1],
+                    'iterations': [30, 50, 100]
+                },
+                "AdaBoost Regression":{
+                    'learning_rate':[.1,.01,0.5,.001],
+                    'n_estimators': [8,16,32,64,128,256]
+                }
+            }
 
-            model_report:dict=evaluate_model(X_train=X_train, y_train=y_train, X_test=X_test, y_test=y_test, models=models)
+            model_report:dict=evaluate_model(X_train=X_train, y_train=y_train, X_test=X_test, y_test=y_test, models=models, params=params)
 
             best_model_score = max(sorted(model_report.values()))
 
--- src/utils.py
+++ src/utils.py
@@ -3,6 +3,7 @@ import sys
 import numpy as np
 import pandas as pd
 from src.exception import CustomException
+from sklearn.model_selection import GridSearchCV
 from src.logger import logging
 import dill
 from sklearn.metrics import r2_score
@@ -20,12 +21,17 @@ def save_object(file_path, obj):
         raise CustomException(e, sys)
 
 
-def evaluate_model(X_train, y_train, X_test, y_test, models:dict):
+def evaluate_model(X_train, y_train, X_test, y_test, models:dict, params:dict):
     try:
         report = {}
         for i in range(len(list(models))):
             model = list(models.values())[i]
+            param = params[list(models.keys())[i]]
 
+            clf = GridSearchCV(model, param, cv=3)
+            clf.fit(X_train, y_train)
+
+            model.set_params(**clf.best_params_)
             model.fit(X_train, y_train)
 
             y_test_predicted = model.predict(X_test)
@@ -33,7 +39,7 @@ def evaluate_model(X_train, y_train, X_test, y_test, models:dict):
             test_model_score = r2_score(y_test, y_test_predicted)
 
             report[list(models.keys())[i]] = test_model_score
-            
+
         return report
     
     except Exception as e:
